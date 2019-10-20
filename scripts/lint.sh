09c1fa42243bf4a0d39286401e1cf882ed245c5e
maniek2332 <maniek2332@gmail.com>
2019-10-20 19:00:42 +0200

    updated lint.sh to work with different clang-format binary names

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,10 +2,21 @@
 
 set -e
 
+if which clang-format
+then
+    CLANG_FORMAT=clang-format
+elif which clang-format-7
+then
+    CLANG_FORMAT=clang-format-7
+else
+    echo "clang-format not found!"
+    exit 1
+fi
+
 SOURCES=$(find . -type f \( -name "*.h" -o -name "*.cpp" \) \
         -not \( -path "./build/*" -o -path "./third_party/*" \))                          
-echo $(clang-format -version)
-clang-format -i ${SOURCES}
+${CLANG_FORMAT} -version
+${CLANG_FORMAT} -i ${SOURCES}
 DIFF=$(git diff)
 
 if [ -n "${DIFF}" ]; then
