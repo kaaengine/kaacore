818472ea1b0201cf27da9ea89baa006b3d221900
maniek2332 <maniek2332@gmail.com>
2019-10-21 23:45:44 +0200

    linting

--- src/audio.cpp
+++ src/audio.cpp
@@ -13,7 +13,6 @@ namespace kaacore {
 const uint32_t event_music_finished = SDL_RegisterEvents(1);
 const uint16_t default_mixing_channels_count = 32;
 
-
 SoundData::SoundData(Mix_Chunk* raw_sound) : _raw_sound(raw_sound) {}
 
 SoundData::~SoundData()
@@ -99,27 +98,26 @@ void
 Music::play(double volume_factor)
 {
     KAACORE_ASSERT(get_engine()->audio_manager);
-    get_engine()->audio_manager->play_music(*this, this->volume * volume_factor);
+    get_engine()->audio_manager->play_music(
+        *this, this->volume * volume_factor);
 }
 
-
-void _music_finished_hook()
+void
+_music_finished_hook()
 {
     SDL_Event event;
     event.type = event_music_finished;
     SDL_PushEvent(&event);
 }
 
-
 AudioManager::AudioManager() : master_sound_volume(1.), master_music_volume(1.)
 {
     SDL_InitSubSystem(SDL_INIT_AUDIO);
     Mix_Init(0); // no libraries, just WAV support
-    auto err_code = Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048);
+    auto err_code =
+        Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048);
     if (err_code == -1) {
-        log<LogLevel::error>(
-            "Failed to open audio (%s)", Mix_GetError()
-        );
+        log<LogLevel::error>("Failed to open audio (%s)", Mix_GetError());
         return;
     }
     Mix_HookMusicFinished(_music_finished_hook);
@@ -162,13 +160,10 @@ AudioManager::play_sound(const Sound& sound, const double volume_factor)
     if (sound._sound_data->_raw_sound) {
         auto channel = Mix_PlayChannel(-1, sound._sound_data->_raw_sound, 0);
         if (channel < 0) {
-            log<LogLevel::error>(
-                "Failed to play sound (%s)", Mix_GetError()
-            );
+            log<LogLevel::error>("Failed to play sound (%s)", Mix_GetError());
             Mix_Volume(
                 channel,
-                this->master_sound_volume * volume_factor * MIX_MAX_VOLUME
-            );
+                this->master_sound_volume * volume_factor * MIX_MAX_VOLUME);
             return;
         }
     } else {
@@ -183,12 +178,11 @@ AudioManager::play_music(const Music& music, const double volume_factor)
     if (music._music_data->_raw_music) {
         auto err_code = Mix_PlayMusic(music._music_data->_raw_music, 1);
         if (err_code == -1) {
-            log<LogLevel::error>(
-                "Failed to play music (%s)", Mix_GetError()
-            );
+            log<LogLevel::error>("Failed to play music (%s)", Mix_GetError());
             return;
         }
-        Mix_VolumeMusic(this->master_music_volume * volume_factor * MIX_MAX_VOLUME);
+        Mix_VolumeMusic(
+            this->master_music_volume * volume_factor * MIX_MAX_VOLUME);
         this->current_music = music;
     } else {
         log<LogLevel::error>("Failed to played incorrectly loaded music");
@@ -207,12 +201,14 @@ AudioManager::music_state()
     }
 }
 
-uint16_t AudioManager::mixing_channels() const
+uint16_t
+AudioManager::mixing_channels() const
 {
     return Mix_AllocateChannels(-1);
 }
 
-void AudioManager::mixing_channels(const uint16_t channels)
+void
+AudioManager::mixing_channels(const uint16_t channels)
 {
     Mix_AllocateChannels(channels);
 }
