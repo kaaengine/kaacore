a0eba355f48cfd755018bd28b05ed3d872e6d29a
labuzm <labuzm@gmail.com>
2019-12-10 01:02:09 +0100

    fix format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -14,9 +14,8 @@ else
 fi
 
 SCRIPTS_DIR=${BASH_SOURCE%/*}
-SOURCES=$(find "${SCRIPTS_DIR}" -type f \( -name "*.h" \
-        -o -name "*.cpp" \) -not \( -path "${SCRIPTS_DIR}/../build/*" \
-        -o -path "${SCRIPTS_DIR}/../third_party/*" \))                          
+SOURCES=$(find "${SCRIPTS_DIR}/.." -type f \( -name "*.h" -o -name "*.cpp" \) \
+        -not \( -path "${SCRIPTS_DIR}/../build/*" -o -path "${SCRIPTS_DIR}/../third_party/*" \))   
 ${CLANG_FORMAT} -version
 ${CLANG_FORMAT} -i ${SOURCES}
 DIFF=$(git diff)
