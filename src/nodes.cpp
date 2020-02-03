353bc188f5913b5d4e3065b65a5d1445410f92fb
maniek2332 <maniek2332@gmail.com>
2020-02-03 22:03:05 +0100

    fix for template deduction fail on macos build

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -286,7 +286,7 @@ Node::absolute_rotation()
         this->_recalculate_model_matrix_cumulative();
     }
 
-    return DecomposedTransformation(this->_model_matrix.value).rotation;
+    return DecomposedTransformation<float>(this->_model_matrix.value).rotation;
 }
 
 void
@@ -313,7 +313,7 @@ Node::absolute_scale()
         this->_recalculate_model_matrix_cumulative();
     }
 
-    return DecomposedTransformation(this->_model_matrix.value).scale;
+    return DecomposedTransformation<float>(this->_model_matrix.value).scale;
 }
 
 void
