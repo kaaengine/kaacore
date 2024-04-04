cfaabf15a5cfea3e02c6f91482bdbfd5f17eab2c
maniek2332 <maniek2332@gmail.com>
2024-04-04 23:31:18 +0200

    Added `UnicodeBuffer`/`UnicodeView` types, option to include additional characters to font bitmap.

--- include/kaacore/fonts.h
+++ include/kaacore/fonts.h
@@ -14,6 +14,7 @@
 #include "kaacore/resources.h"
 #include "kaacore/shapes.h"
 #include "kaacore/textures.h"
+#include "kaacore/unicode_buffer.h"
 
 namespace kaacore {
 
@@ -22,8 +23,7 @@ initialize_fonts();
 void
 uninitialize_fonts();
 
-typedef std::vector<stbtt_packedchar> BakedFontData;
-typedef uint32_t UnicodeCodepoint;
+typedef std::unordered_map<UnicodeCodepoint, stbtt_packedchar> BakedFontData;
 
 // width of created font atlas texture
 const size_t font_baker_texture_width = 2048;
@@ -43,6 +43,23 @@ const int font_sdf_edge_value = 180;
 // how much field value increases/decreases over 1 pixel
 const float font_sdf_pixel_dist_scale =
     font_sdf_edge_value / double(font_sdf_padding);
+
+struct FontResourcesRegistryKey {
+    std::string path;
+    UnicodeBuffer additional_codepoints;
+
+    FontResourcesRegistryKey() = default;
+
+    FontResourcesRegistryKey(const std::string& path, const UnicodeView additional_codepoints)
+        : path(path), additional_codepoints(additional_codepoints)
+    {}
+
+    bool operator==(const FontResourcesRegistryKey& other) const
+    {
+        return this->path == other.path &&
+               this->additional_codepoints.view() == other.additional_codepoints.view();
+    }
+};
 
 struct FontMetrics {
     FontMetrics() = default;
@@ -88,28 +105,29 @@ struct FontRenderGlyph {
 class FontData : public Resource {
   public:
     const std::string path;
+    UnicodeBuffer additional_codepoints;
     BakedFontData baked_font;
     ResourceReference<Texture> baked_texture;
     FontMetrics font_metrics;
 
     ~FontData();
-    static ResourceReference<FontData> load(const std::string& path);
+    static ResourceReference<FontData> load(const std::string& path, const UnicodeView additional_codepoints);
     static ResourceReference<FontData> load_from_memory(const Memory& memory);
     Shape generate_text_shape(
         const std::string& text, double size, double indent, double max_width);
     std::vector<FontRenderGlyph> generate_render_glyphs(
-        const std::string& text, const double scale_factor);
+        const UnicodeView text, const double scale_factor);
     inline FontMetrics metrics() { return this->font_metrics; }
 
   private:
-    FontData(const std::string& path);
+    FontData(const std::string& path, const UnicodeView additional_codepoints);
     FontData(
         const ResourceReference<Texture> baked_texture,
         const BakedFontData baked_font, const FontMetrics font_metrics);
     virtual void _initialize() override;
     virtual void _uninitialize() override;
 
-    friend class ResourcesRegistry<std::string, FontData>;
+    friend class ResourcesRegistry<FontResourcesRegistryKey, FontData>;
     friend void initialize_fonts();
     friend void uninitialize_fonts();
 };
@@ -120,6 +138,7 @@ class Font {
   public:
     Font();
     static Font load(const std::string& font_filepath);
+    static Font load(const std::string& font_filepath, const UnicodeView additional_codepoints);
 
     bool operator==(const Font& other);
 
@@ -139,7 +158,7 @@ Font&
 get_default_font();
 
 class TextNode {
-    std::string _content;
+    UnicodeBuffer _content;
     double _font_size;
     double _line_width;
     double _interline_spacing;
@@ -154,7 +173,8 @@ class TextNode {
     TextNode();
     ~TextNode();
 
-    std::string content() const;
+    const UnicodeView content() const;
+    void content(const UnicodeView content);
     void content(const std::string& content);
 
     double font_size() const;
@@ -187,3 +207,18 @@ struct hash<kaacore::Font> {
     }
 };
 }
+
+
+namespace std {
+using kaacore::FontResourcesRegistryKey;
+using kaacore::hash_combined;
+
+template<>
+struct hash<FontResourcesRegistryKey> {
+    size_t operator()(const FontResourcesRegistryKey& key) const
+    {
+        return hash_combined(key.path, key.additional_codepoints.view());
+    }
+};
+
+} // namespace std
